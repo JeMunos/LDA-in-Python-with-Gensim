bc5e6c6a32a15d62e0d49f2ffe3ba3e4741f6545
Jesse Munos <jemunos@cisco.com>
2017-04-26 19:33:41 -0500

    hopefully final

--- dict_parser.py
+++ dict_parser.py
@@ -11,6 +11,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 final_dict={}
 dll_dict = {}
 dll_list = []
+ioc_title_list=[]
+first_row_imports=[]
+first_row_ioc=[]
+first_rwo_combined=[]
+
+
+
 path = 'files/*.json'
 #path = '../Malware_Project/TG_Json_Data/*.json'
 size = len(glob.glob(path))
@@ -23,8 +30,8 @@ for file in glob.glob(path):
         data_file.close()
 
 #Get the sha from the bottom fo the file and store it in sha variable
-    sha = json_data['status']['sha256']
-
+    sha = str(json_data['status']['sha256'])
+    
 #find the artifact value that contains our sha, store the list of dictionaries in oper_dict_list
     for k,v in json_data['artifacts'].items():
         if json_data['artifacts'][k]['sha256'] == sha:
@@ -32,7 +39,9 @@ for file in glob.glob(path):
     for d in oper_dict_list:
         if sha not in final_dict.keys():
             final_dict[sha] = {}
-        final_dict[sha][str(d['dll'])] = [str(n[0]) for n in d['entries']] #for each item in oper_dict_list create final_dict{<sha>: {<file.dll>: [import1, import2, etc]}}
+            final_dict[sha]['dll'] = {}
+            final_dict[sha]['iocs'] = {}
+        final_dict[sha]['dll'][str(d['dll'])] = [str(n[0]) for n in d['entries']] #for each item in oper_dict_list create final_dict{<sha>: {<file.dll>: [import1, import2, etc]}}
         #if dll_dict doesn't contain a dll, add it to the dll_dict and populate the import entries
         #if dll_dict does contain the dll, check all imports and add new imports as necessary
         if d['dll'] not in dll_dict.keys():
@@ -44,16 +53,106 @@ for file in glob.glob(path):
         #build list of all DLL's contained in the dll_dict
         if d['dll'] not in dll_list:
             dll_list.append(str(d['dll']))
-#print("dll dict ", dll_dict)
-#print("dll_list", dll_list)
+
+
+#Get all IOC titles and store in list to add to final dictionary if title is unique
+    ioc_title_list.append([str(n['title']) for n in json_data['iocs'] if n['title'] not in ioc_title_list])
+#creates master list of iocs for each sha            
+    final_dict[sha]['iocs'] = [str(n['title']) for n in json_data['iocs']]
+    
+ioc_title_list = [n for x in ioc_title_list for n in x]    
 first_row = [''] + [[k] + v for k,v in dll_dict.items()]
-first_row = [n for x in first_row for n in x]
+first_row_imports = [n for x in first_row for n in x]
+first_row_ioc = ioc_title_list
+first_row_combined = first_row_imports + first_row_ioc
+
+print("IOC LIST: ", ioc_title_list)
 
 
 #ohh god ohh god CSV....fuck
 with open('output/imports.csv', 'w') as f:
     writer = csv.writer(f)
-    writer.writerow([' '] + first_row)
-    for value in dll_list:
-        
+    writer.writerow([' '] + first_row_imports)
+    for sha in final_dict.keys():
+        row =[]
+        row.append(sha)
+        dll_exists = False
+        #this will keep track of the true false of a dll existing in final_dict, also it will append a 1 if the dll does exist and a 0 if it does not exist for a given SHA
+        for item in first_row_imports:
+            if '.dll' in item:
+                dll = item
+                if dll in final_dict[sha]['dll'].keys():
+                    row.append('1')
+                    dll_exists = True
+                else:
+                    row.append('0')
+                    dll_exists = False
+            #this case will track import statments, 0 if the dll doens't exist. if the dll does exist, iterate over hte list and mark 1 or 0 where appropriate
+            else:
+                if dll_exists:
+                    if item in final_dict[sha]['dll'][dll]:
+                        row.append('1')
+                    else:
+                        row.append('0')
+                else:
+                    row.append('0')
+        writer.writerow(row)
+
+with open('output/ioc.csv','w') as f:
+    writer = csv.writer(f)
+    writer.writerow([' '] + first_row_ioc)
+    for sha in final_dict.keys():
+        row = []
+        row.append(sha)
+        for item in first_row_ioc:
+            if item in final_dict[sha]['iocs']:
+                row.append('1')
+            else:
+                row.append('0')
+        writer.writerow(row)
+
+with open('output/combine.csv','w') as f:
+    writer = csv.writer(f)
+    writer.writerow([' '] + first_row_combined)
+    for sha in final_dict.keys():
+        row=[]
+        row.append(sha)
+        dll_exists = False
+        ioc_hit = False
+        for item in first_row_combined:
+            if ioc_title_list[0] == item:
+                ioc_hit=True
+            if not ioc_hit:
+                if '.dll' in item:
+                    dll = item
+                    if dll in final_dict[sha]['dll'].keys():
+                        row.append('1')
+                        dll_exists = True
+                    else:
+                        row.append('0')
+                        dll_exists = False
+                else:
+                    if dll_exists:
+                        if item in final_dict[sha]['dll'][dll]:
+                            row.append('1')
+                        else:
+                            row.append('0')
+                    else:
+                        row.append('0')
+            else:
+                if item in final_dict[sha]['iocs']:
+                    row.append('1')
+                else:
+                    row.append('0')
+        writer.writerow(row)
+
+
+
+
+
+
+
+
+
+
 
